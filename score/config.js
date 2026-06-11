1ff2dc7bf38c9cac733a90df1da5b047aba4adb1
asdasd121208-arch <asdasd121208@gmail.com>
2026-06-11 22:15:06 +0900

    Update config.js

--- score/config.js
+++ score/config.js
@@ -1,2 +1,2 @@
 // config.js
-const GAS_API_URL = "https://script.google.com/macros/s/XXXXX/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyc5q89gS09r-1dbWxI4o7RpkWEZ_RW2BhxlkxmkQPFUQLrSjKkDUztR6uNsB_TtA1jUA/exec";
